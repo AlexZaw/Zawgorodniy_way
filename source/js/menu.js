2e0e9f4bf1a18cc0682a359a76c64b554a875232
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-07-28 04:29:13 +0700

    fix: BEM naming errors and clean comments

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,11 +1,11 @@
-const menuControl = document.querySelector('.main-nav__control');
+const menuControl = document.querySelector('.menu-control');
 const menu = document.querySelector('.main-nav');
 document.body.classList.remove('no-js');
-menuControl.classList.remove('main-nav__control--hidden');
+menuControl.classList.remove('menu-control--hidden');
 
 const showHideMenu = () =>{
-  menuControl.classList.toggle('main-nav__control--close');
-  if(menuControl.classList.contains('main-nav__control--close')){
+  menuControl.classList.toggle('menu-control--close');
+  if(menuControl.classList.contains('menu-control--close')){
     menuControl.setAttribute('aria-label', 'Закрыть мобильное меню');
   }else{
     menuControl.setAttribute('aria-label', 'Открыть мобильное меню');
